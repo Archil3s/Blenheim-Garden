2a77d0894f82d95c521eb68af2e443d9ec9e16f7
Archil3s <82018448+Archil3s@users.noreply.github.com>
2026-08-22 12:13:39 +1200

    Load horizontal palette workspace styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ import "./growveg-row-draw.css";
 import "./growveg-modifier-keys.css";
 import "./botanical-plant-icons.css";
 import "./botanical-zoom-detail.css";
+import "./palette-workspace.css";
 import "./blenheim-season-guide.css";
 
 export const metadata: Metadata = {
