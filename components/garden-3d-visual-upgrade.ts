00f01bf1c873d27ffe0a2ce8314ab4bde7aa1e2a
Archil3s <82018448+Archil3s@users.noreply.github.com>
2026-09-15 21:00:41 +1200

    feat(3d): provide single visual upgrade import

---
+++ components/garden-3d-visual-upgrade.ts
@@ -0,0 +1,7 @@
+export * from "@/components/garden-3d-upgrade-index";
+export * from "@/components/garden-3d-icon-scale";
+export * from "@/components/garden-3d-crop-height";
+export * from "@/components/garden-3d-performance";
+export * from "@/components/garden-3d-wood-detail";
+export * from "@/components/garden-3d-soil-detail";
+export * from "@/components/garden-3d-camera";
